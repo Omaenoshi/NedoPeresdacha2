f139cacf687ec6bafbfab693e069be3f7cfaed3a
Omaenoshi <rassvetalov.maksim@mail.ru>
2021-05-25 23:22:51 +0500

    Add

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NedoExam2
+{
+    class Person
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public string BirthDate { get; private set; }
+
+
+
+        public Person(string id, string firstName, string lastName, string birstDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birstDate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NedoExam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var peopleInfo = File.ReadAllLines(@"../../../People.txt");
+        } 
+    }
+}
